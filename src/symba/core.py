8fb58668523b1d680289c8dc2d29706571af895a
James Ashford <ashfordj@rocketmail.com>
2026-02-27 12:50:49 +0000

    Simplified the replication candidate method and improved documentation/variable naming

--- src/symba/core.py
+++ src/symba/core.py
@@ -2,46 +2,84 @@ import numpy as np
 
 
 def gather_replication_candidate(state: np.ndarray) -> list[set]:
-    size = len(state)
-    candidates = [set() for _ in range(size)]
+    """Gather the replication candidate values at each position in state after all positions attempt replication.
+
+    For each currently non-zero valued position in the state, it's value becomes a "replication candidate" for a
+    target position, equal to the current position plus the current value.
+
+    If that target position is occupied by a non-zero value in the current state, the original value will also
+    becomes a "replication candidate" for a new target position equal to it's current position plus the value in the
+    current target position.
+
+    This process is repeated until the new target position is occupied by an already encountered position or a zero.
+
+    e.g. a 4 at position 2 will become a replication candidate for position 6 (2 + 4). If position 6 is occupied by a
+    -1 in the current state, the 4 will also become a replication candidate for position 1 (2 + (-1)). If position 1
+    is occupied by a 0, 4, or -1 the replication attempts terminate.
+
+    Parameters
+    ---------
+    state
+        Current 1D state of all positions, shape=(num_states,)
+
+    Returns
+    -------
+    candidates
+        List of replication candidate values for each position in the 1D state
+
+    """
+    (num_states,) = state.shape
+    # Create a set of candidates for each position
+    candidates = [set() for _ in range(num_states)]
     for pos, current_val in enumerate(state):
-        # skip if current value is zero as nothing happens
-        if current_val == 0:
-            continue
-        # gather all the offsets that this current_value will be a candidate for
-        offsets = {current_val}
-        offset_queue = [current_val]
-        while offset_queue:
-            check_offset = offset_queue.pop()
-            value_at_offset = state[(pos + check_offset) % size]
-            if value_at_offset not in offsets:
-                offsets.add(value_at_offset)
-                offset_queue.append(value_at_offset)
+        # Store all offsets already checked
+        offsets_checked = {current_val}
+        offset_to_check = current_val
+        while offset_to_check != 0:
+            # Get target position and it's value
+            target_position = (pos + offset_to_check) % num_states
+            target_value = state[target_position]
+            # Add original value as a candidate for this position
+            candidates[target_position].add(current_val)
+            # Decide whether to continue searching
+            if target_value not in offsets_checked:
+                offsets_checked.add(target_value)
+                offset_to_check = target_value
             else:
-                break
-        # add as a candidate at all those offsets
-        for offset in offsets:
-            new_pos = (pos + offset) % size
-            if offset != 0:
-                candidates[new_pos].add(int(current_val))
+                offset_to_check = 0
     return candidates
 
 
 def norm_zero(
-    current_state: np.ndarray,
-    collisions: list[set],
+    state: np.ndarray,
+    candidates: list[set],
 ) -> int:
-    return np.array([list(vals)[0] if len(vals) == 1 else 0 for vals in collisions])
+    """Barricelli's 0 Norm.
+    Resolve replication candidates by setting state to 0 in all cases except only 1 candidate.
+
+    Parameters
+    ----------
+    state:
+        Current 1D state of all positions, shape=(num_states,) [Not used in this `norm`]
+    candidates:
+        Replication candidates for each position in state (see `gather_replication_candidate` for details.)
+
+    Returns
+    -------
+    new_state:
+        New 1D state after resolving replication behaviour
+    """
+    return np.array([list(vals)[0] if len(vals) == 1 else 0 for vals in candidates])
 
 
 def norm_A(
     current_state: np.ndarray,
-    collisions: list[set],
+    candidates: list[set],
 ):
     # If no collision, return the value that got written to that position
     size = len(current_state)
     new_state = np.zeros_like(current_state)
-    for ind, (candidates, current_val) in enumerate(zip(collisions, current_state)):
+    for ind, (candidates, current_val) in enumerate(zip(candidates, current_state)):
         if len(candidates) == 1:
             new_state[ind] = list(candidates)[0]
         elif len(candidates) == 0:
@@ -72,12 +110,12 @@ def norm_A(
 
 def norm_B(
     current_state: np.ndarray,
-    collisions: list[set],
+    candidates: list[set],
 ):
     # If no collision, return the value that got written to that position
     size = len(current_state)
     new_state = np.zeros_like(current_state)
-    for ind, (candidates, current_val) in enumerate(zip(collisions, current_state)):
+    for ind, (candidates, current_val) in enumerate(zip(candidates, current_state)):
         if len(candidates) == 1:
             new_state[ind] = list(candidates)[0]
         elif len(candidates) == 0:
@@ -108,12 +146,12 @@ def norm_B(
 
 def norm_C(
     current_state: np.ndarray,
-    collisions: list[set],
+    candidates: list[set],
 ):
     # If no collision, return the value that got written to that position
     size = len(current_state)
     new_state = np.zeros_like(current_state)
-    for ind, (candidates, current_val) in enumerate(zip(collisions, current_state)):
+    for ind, (candidates, current_val) in enumerate(zip(candidates, current_state)):
         if len(candidates) == 1:
             new_state[ind] = list(candidates)[0]
         elif len(candidates) == 0:
@@ -135,12 +173,12 @@ def norm_C(
 
 def norm_D(
     current_state: np.ndarray,
-    collisions: list[set],
+    candidates: list[set],
 ):
     # If no collision, return the value that got written to that position
     size = len(current_state)
     new_state = np.zeros_like(current_state)
-    for ind, (candidates, current_val) in enumerate(zip(collisions, current_state)):
+    for ind, (candidates, current_val) in enumerate(zip(candidates, current_state)):
         if len(candidates) == 1:
             new_state[ind] = list(candidates)[0]
         elif len(candidates) == 0:
